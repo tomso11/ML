383db4fc04dc963e88ca14838b16ee96b834e316
tsoracco <tsoracco@itba.edu.ar>
2020-08-06 15:38:20 -0300

    split utils

---
+++ intro/utils.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import math
+
+def split_array(array, testsize=0.25, trainsize=0.75):
+    if( testsize + trainsize != 1.0 ):
+        testsize = 0.25
+        trainsize = 0.75
+    samples = len(array)
+    ntrain = math.ceil(samples*trainsize)
+    ntest = math.floor(samples*testsize)
+    train = array[:ntrain]
+    test = array[(samples-ntest):]
+
+    return train, test
+
+def split_df(df, testsize=0.25, trainsize=0.75):
+    if( testsize + trainsize != 1.0 ):
+        testsize = 0.25
+        trainsize = 0.75
+    samples = df.shape[0]
+    ntrain = math.floor(samples*trainsize)
+    ntest = samples - ntrain;
+    train = df.loc[:(ntrain-1), :]
+    test = df.loc[(samples-ntest):, :]
+
+    return train, test
+
+# test
+
+# ary = [0, 1, 2, 3, 4, 5]
+# a, b = split_array(ary, testsize=0.4, trainsize=0.6)
+# print(a)
+# print(b)
+
+df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
+c, d = split_df(df, testsize=0.5,trainsize=0.5)
+print(c.shape[0])
+print(d.shape[0])
